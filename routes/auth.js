62326aee71671258a50ec28d99a26e6e9bf4795e
Suhara Vithanage <suha.lyoko@hotmail.com>
2021-06-05 22:03:54 +0530

    outputting the error

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,35 @@
 const router = require("express").Router();
+const User = require("../model/user");
+const { registerValidation } = require("../validation");
+// const Joi = require("@hapi/joi");
 
-router.post("/register", (req, res) => {
-    //print this
-  res.send("Register");
+// const schema = Joi.object({
+//     name: Joi.string().required(),
+//     email: Joi.string().min(6).required().email(),
+//     password: Joi.string().min(6).required()
+//   });
+
+router.post("/register", async (req, res) => {
+  //validate and then print the error
+//   const validation = schema.validate(req.body);
+//   res.send(validation);
+  const { error } = registerValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password
+  });
+  try {
+    const savedUser = await user.save();
+    res.send(savedUser);
+  } catch (err) {
+    res.status(404).send(err);
+  }
 });
 
-// router.post('/login')
-
 module.exports = router;
+
+// print this
+//   res.send("Register");
