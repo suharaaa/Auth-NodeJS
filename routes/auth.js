254e4f0c761cb4966e6f7252012602c31b69f5f1
Suhara Vithanage <suha.lyoko@hotmail.com>
2021-06-06 23:31:33 +0530

    checking user already exists

--- routes/auth.js
+++ routes/auth.js
@@ -1,25 +1,21 @@
 const router = require("express").Router();
 const User = require("../model/user");
 const { registerValidation } = require("../validation");
-// const Joi = require("@hapi/joi");
-
-// const schema = Joi.object({
-//     name: Joi.string().required(),
-//     email: Joi.string().min(6).required().email(),
-//     password: Joi.string().min(6).required()
-//   });
 
 router.post("/register", async (req, res) => {
   //validate and then print the error
-//   const validation = schema.validate(req.body);
-//   res.send(validation);
   const { error } = registerValidation(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
+  //check user is already in db
+  const emailExist = await User.findOne({email: req.body.email});
+  if(emailExist) return res.status(400).send('Email already exists');
+
+  //create a new user
   const user = new User({
     name: req.body.name,
     email: req.body.email,
-    password: req.body.password
+    password: req.body.password,
   });
   try {
     const savedUser = await user.save();
